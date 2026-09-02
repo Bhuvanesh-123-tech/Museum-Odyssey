b5d568ffce6852565e79d1fca2c617bfe8934d60
Bhuvanesh-123-tech <bbgamingsub@gmail.com>
2026-09-02 22:52:28 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,25 @@
-const yeahBtn = document.getElementById("yeahBtn");
+import * as THREE from 'https://unpkg.com/three@0.168.0/build/three.module.js';
 
-yeahBtn.addEventListener("click", () => {
-    alert("Entering Museum...");
-});
+const scene = new THREE.Scene();
+
+const camera = new THREE.PerspectiveCamera(
+    75,
+    window.innerWidth / window.innerHeight,
+    0.1,
+    1000
+);
+
+const renderer = new THREE.WebGLRenderer();
+
+renderer.setSize(window.innerWidth, window.innerHeight);
+
+document.body.appendChild(renderer.domElement);
+
+camera.position.set(0, 2, 5);
+
+function animate() {
+    requestAnimationFrame(animate);
+    renderer.render(scene, camera);
+}
+
+animate();
